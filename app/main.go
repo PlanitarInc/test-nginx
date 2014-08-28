2b80106b9cb5ce455746d61d710efaea25fc7b64
korya <korya@github.com>
2014-08-28 13:31:44 -0400

    Add basic app mockup

---
+++ app/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/PlanitarInc/context"
+	"github.com/PlanitarInc/sessions"
+	"github.com/PlanitarInc/walk-inside-api/util/jsonutil"
+	"github.com/PlanitarInc/web"
+	"github.com/rlmcpherson/s3gof3r"
+)
+
+var (
+	sessionid = "SID"
+	hashKey   = []byte("k_F3X5@H#pJbHmJlhN)^440&fQE&w1!1")
+	blockKey  = []byte("lhdar^-n*pMkraUJoPYWV6^XfCJF$D4$")
+	store     = sessions.NewCookieStore(hashKey, blockKey)
+
+	db *sql.DB
+)
+
+type baseCtx struct{}
+
+type apiCtx struct {
+	*baseCtx
+	replier  jsonutil.JsonWriter
+	sessions map[string]*sessions.Session
+}
+
+func (ctx *baseCtx) contextClear(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	fmt.Printf("%s %s\n", req.Method, req.URL.Path)
+	next(rw, req)
+	/* XXX Gorilla's sessions use global `context`, need to clean it manually. */
+	context.Clear(req.Request)
+	fmt.Println("")
+}
+
+func (ctx *apiCtx) setSessions(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	ctx.sessions = make(map[string]*sessions.Session)
+
+	next(rw, req)
+
+	/* XXX Clean? */
+	ctx.sessions = make(map[string]*sessions.Session)
+}
+
+func (ctx *apiCtx) setJsonWriter(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	ctx.replier = jsonutil.New(rw)
+
+	next(rw, req)
+
+	fmt.Println("emitting..............")
+	ctx.replier.Emit()
+}
+
+func (ctx *apiCtx) getSession(name string, rw web.ResponseWriter, req *web.Request) *sessions.Session {
+	if session, ok := ctx.sessions[name]; ok {
+		fmt.Printf("session: session '%s' is alread loaded\n", name)
+		return session
+	}
+
+	session, err := store.Get(req.Request, name)
+	if err != nil {
+		fmt.Printf("session: sessionid get error: %s\n", err)
+		return session
+	}
+	fmt.Printf("load session: %v\n", session)
+	ctx.sessions[name] = session
+	return session
+}
+
+func (ctx *apiCtx) removeSession(name string, rw web.ResponseWriter, req *web.Request) {
+	session, ok := ctx.sessions[name]
+	if !ok {
+		fmt.Printf("cannot find session: %s\n", name)
+		return
+	}
+
+	fmt.Printf("remove session: %v\n", session)
+	/* Remove all values and invalidate the cookie */
+	session.Values = make(map[interface{}]interface{})
+	session.Options.MaxAge = -1
+}
+
+func (ctx *apiCtx) saveSession(name string, rw web.ResponseWriter, req *web.Request) {
+	session, ok := ctx.sessions[name]
+	if !ok {
+		fmt.Printf("cannot find session: %s\n", name)
+		return
+	}
+
+	fmt.Printf("save session: %v\n", session)
+	if err := session.Save(req.Request, rw); err != nil {
+		fmt.Printf("cannot save session: %s\n", err)
+	}
+}
+
+type viewCtx struct {
+	*apiCtx
+	viewId string
+}
+
+func (ctx *viewCtx) loadSession(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	ctx.getSession("qwe", rw, req)
+	next(rw, req)
+	ctx.saveSession("qwe", rw, req)
+}
+
+func (ctx *viewCtx) authenticated(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	/* XXX send some query, wait for the response, we don't really care what
+	 * the reponse is.
+	 */
+	var s sql.NullString
+	if err := db.QueryRow(`SELECT pg_sleep(0.1)`).Scan(&s); err != nil {
+		ctx.replier.SetError(err, http.StatusInternalServerError)
+		return
+	}
+
+	next(rw, req)
+}
+
+func (ctx *viewCtx) authorized(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	if _, ok := req.PathParams["key"]; !ok {
+		ctx.replier.SetError("could not find key", http.StatusInternalServerError)
+		return
+	}
+
+	next(rw, req)
+}
+
+func (ctx *viewCtx) get(rw web.ResponseWriter, req *web.Request) {
+	key, _ := req.PathParams["key"]
+
+	if err := getS3Object(key, rw); err != nil {
+		ctx.replier.SetError(err, http.StatusInternalServerError)
+		return
+	}
+
+	ctx.replier.SetObj(nil, 200)
+}
+
+func getS3Object(key string, rw web.ResponseWriter) error {
+	conf := new(s3gof3r.Config)
+	*conf = *s3gof3r.DefaultConfig
+	conf.Md5Check = false
+
+	k, err := s3gof3r.EnvKeys()
+	if err != nil {
+		return err
+	}
+
+	s3 := s3gof3r.New(get.EndPoint, k)
+	b := s3.Bucket(get.Bucket)
+
+	s3gof3r.SetLogger(os.Stderr, "", log.LstdFlags, get.Debug)
+
+	r, header, err := b.GetReader(key, conf)
+	if err != nil {
+		return err
+	}
+	log.Println("Headers: ", header)
+	if _, err = io.Copy(rw, r); err != nil {
+		return err
+	}
+	if err = r.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	router := web.New(baseCtx{})
+	router.Middleware((*baseCtx).contextClear)
+
+	router.Middleware(web.StaticMiddleware("static"))
+
+	apiRouter := router.Subrouter(apiCtx{}, "/api/v1")
+	/* This is a little bit unexpected, but the order of middleware invocation
+	 * is the opposite of the order of middleware definition. Hence
+	 * Session middleware should come before the Json Writer middleware,
+	 * because session cookies are to be set before anything is emitted.
+	 */
+	apiRouter.Middleware((*apiCtx).setSessions)
+	apiRouter.Middleware((*apiCtx).setJsonWriter)
+
+	/* Configure the endpoints */
+	viewRouter := apiRouter.Subrouter(viewCtx{}, "/view")
+	viewRouter.Middleware((*viewCtx).loadSession)
+	viewRouter.Middleware((*viewCtx).authenticated)
+	viewRouter.Middleware((*viewCtx).authorized)
+	viewRouter.Get(":key", (*viewCtx).get)
+
+	/* Connect to DB */
+	db, err := sql.Open("postgres", "dbname=iguidedb host=localhost user=view_api")
+	if err != nil {
+		panic(err)
+	}
+
+	http.ListenAndServe(":9000", router)
+}
